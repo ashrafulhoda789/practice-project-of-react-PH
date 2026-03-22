951aab39849e76cd6e8c963128a37543820b92d8
asrafulhoda789 <asrafulhoda789@gmail.com>
2026-03-22 23:08:41 +0600

    project setup with tailwind and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import DaisyNavbar from './Components/DaisyNav/DaisyNavbar'
+
+function App() {
+
+  return (
+    <>
+
+      <header>
+        <DaisyNavbar></DaisyNavbar>
+      </header>
+
+      <main className='w-11/12 mx-auto'>
+        
+      </main>
+      
+    </>
+  )
+}
+
+export default App
